6cf3fa15498055254a88c736550540484514b082
Rias Van der Veken <hello@rias.be>
2014-04-03 19:32:21 +0200

    Added videocontroller
    Signed-off-by: Rias Van der Veken <hello@rias.be>

---
+++ public/js/controllers/video.js
@@ -0,0 +1,8 @@
+// https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&order=viewCount&q=One+Republic&type=video&videoCategoryId=10&videoEmbeddable=true&key=AIzaSyDbhofgKeHR6BvGATtxqGv1WIJMSaCl5sM
+'use strict';
+
+angular.module('gymwithmusic.system').controller('VideoController', ['$scope', 'Global', '$location', function ($scope, Global, $location) {
+    $scope.global = Global;
+
+    
+}]);
