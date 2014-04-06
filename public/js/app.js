b1de73443b6835d0f873d126cefc4fab0e48b53b
Rias Van der Veken <hello@rias.be>
2014-04-06 22:01:40 +0200

    Search videos and add them
    Signed-off-by: Rias Van der Veken <hello@rias.be>

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
 
-angular.module('gymwithmusic', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'gymwithmusic.system']);
+angular.module('gymwithmusic', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'gymwithmusic.system', 'ngAnimate']);
 
 angular.module('gymwithmusic.system', []);
--- public/js/controllers/video.js
+++ public/js/controllers/video.js
@@ -3,7 +3,7 @@
 angular.module('gymwithmusic.system').controller('VideoController', ['$scope', 'Global', '$location', '$http', 'Faye', function ($scope, Global, $location, $http, Faye) {
     $scope.global = Global;
     Faye.subscribe('/videos', function(videos){
-      console.log(videos);
+      $scope.allVideos = videos;
     });
 
     $scope.addByUrl = function(url)
@@ -27,49 +27,22 @@ angular.module('gymwithmusic.system').controller('VideoController', ['$scope', '
       angular.forEach(response.data[1], function(item){
         results.push(item[0]);
       });
-      console.log(results);
       return results;
     });
   };
 
     $scope.searchYoutube = function(terms)
     {
-      $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=viewCount&q='+terms+'&type=video&videoCategoryId=10&videoEmbeddable=true&key=AIzaSyDbhofgKeHR6BvGATtxqGv1WIJMSaCl5sM').
+      $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=viewCount&q='+terms+'&type=video&videoCategoryId=10&videoEmbeddable=true&key=AIzaSyDbhofgKeHR6BvGATtxqGv1WIJMSaCl5sM').
       success(function(data) {
-        console.log(data);
-        // var title = data.items[0].snippet.title;
-        // var thumbnail = data.items[0].snippet.thumbnails.high.url;
-
-        // var video = {
-        //   title: title,
-        //   url: "http://www.youtube.com/watch?v=" + id,
-        //   youtube_id: id,
-        //   thumbnail: thumbnail,
-        //   added_by: Global.user._id
-        // }
-
-        // $http.post('/videos', video).
-        //   success(function(data, status, headers, config) {
-        //     if(data.status == "success")
-        //     {
-        //       $scope.global.messages.push({ type: 'success', msg: "Je video is toegevoegd" });
-        //       Faye.publish('/videos', data.message);
-        //     }
-        //     else
-        //     {
-        //       $scope.global.messages.push({ type: 'danger', msg: data.message });
-        //     }
-        //   }).
-        //   error(function(data, status, headers, config) {
-        //     $scope.global.messages.push({ type: 'danger', msg: 'Oeps! De video werd gevonden maar er liep iets fout bij ons. Probeer opnieuw?' });
-        //   });
+        $scope.searchResults = data.items;
       }).
       error(function() {
         $scope.global.messages.push({ type: 'danger', msg: 'Oeps! Er is iets misgelopen bij YouTube, probeer nog eens?' });
       });
     };
 
-    $scope.addById = function(id)
+    $scope.addById = function(id, $event)
     {
       $http.get('https://www.googleapis.com/youtube/v3/videos?part=snippet&id='+id+'&key=AIzaSyDbhofgKeHR6BvGATtxqGv1WIJMSaCl5sM').
       success(function(data) {
@@ -89,12 +62,20 @@ angular.module('gymwithmusic.system').controller('VideoController', ['$scope', '
           success(function(data) {
             if(data.status === 'success')
             {
+              if($event)
+              {
+                angular.element($event.toElement).addClass('added');
+              }
               $scope.global.messages.push({ type: 'success', msg: 'Je video is toegevoegd' });
               Faye.publish('/videos', data.message);
             }
             else
             {
               $scope.global.messages.push({ type: 'danger', msg: data.message });
+              if($event)
+              {
+                angular.element($event.toElement).addClass('added');
+              }
             }
           }).
           error(function() {
