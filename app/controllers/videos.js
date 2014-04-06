2806cccd4a7245d33f768aa09e6debb5b06c3911
Rias Van der Veken <hello@rias.be>
2014-04-06 16:16:31 +0200

    Functions to add and list videos
    Signed-off-by: Rias Van der Veken <hello@rias.be>

---
+++ app/controllers/videos.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Video = mongoose.model('Video');
+
+
+/**
+ * All videos
+ */
+exports.list = function(req, res, next) {
+    res.send("all videos");
+};
+
+/**
+ * Create video
+ */
+exports.add = function(req, res, next) {
+    var video = new Video(req.body);
+    var message = "";
+    video.save(function(err) {
+        if (err) {
+            switch (err.code) {
+                case 11000:
+                case 11001:
+                    message = 'Video is al toegevoegd';
+                    break;
+                default:
+                    message = 'Oeps! Er is iets misgelopen met de databank.';
+            }
+
+            return res.send({
+                status: "error",
+                message: message
+            });
+        }
+        Video.find().populate('added_by').exec(function(err, videos)
+        {
+            return res.send({
+                status: "success",
+                message: videos
+            });
+        });
+    });
+
+};
