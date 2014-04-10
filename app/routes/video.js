32a3a91122eb97468414a0e6064d7ac88dd85673
Rias Van der Veken <hello@rias.be>
2014-04-10 16:20:35 +0200

    New routes for currentVideo
    Signed-off-by: Rias Van der Veken <hello@rias.be>

--- app/routes/video.js
+++ app/routes/video.js
@@ -5,6 +5,8 @@ var videos = require('../controllers/videos');
 
 module.exports = function(app) {
 
+    app.get('/videos/current', videos.getCurrent);
+    app.post('/videos/current', videos.addCurrent);
     app.post('/videos', videos.add);
     app.get('/videos', videos.list);
     app.post('/videos/:id', videos.vote);
