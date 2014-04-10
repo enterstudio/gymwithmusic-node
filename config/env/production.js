f0f1161a7860f1fc712af7dba4f130cb6dc7094c
Rias Van der Veken <hello@rias.be>
2014-04-10 10:43:47 +0200

    Production config for log-in
    Signed-off-by: Rias Van der Veken <hello@rias.be>

--- config/env/production.js
+++ config/env/production.js
@@ -8,26 +8,26 @@ module.exports = {
     facebook: {
         clientID: '221610951296607',
         clientSecret: '20d5850ef7627e6f99a79b24610c91bc',
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+        callbackURL: 'http://gymwithmusic.herokuapp.com/auth/facebook/callback'
     },
     twitter: {
         clientID: '1ppOUZaqZsLllfEEODACpKW5Z',
         clientSecret: 'EFdn3MZstZftXZWCMGmltYH452dgxiOyfZBoAzRxhOomKk8NFp',
-        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+        callbackURL: 'http://gymwithmusic.herokuapp.com/auth/twitter/callback'
     },
     github: {
         clientID: 'APP_ID',
         clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/github/callback'
+        callbackURL: 'http://gymwithmusic.herokuapp.com/auth/github/callback'
     },
     google: {
         clientID: '280257166673-5rsdlga81l7aoc8gi39vo002rq8jr4h3.apps.googleusercontent.com',
         clientSecret: 't9xw8-N7x9gVCI6JAtnM2enI',
-        callbackURL: 'http://localhost:3000/auth/google/callback'
+        callbackURL: 'http://gymwithmusic.herokuapp.com/auth/google/callback'
     },
     linkedin: {
         clientID: 'API_KEY',
         clientSecret: 'SECRET_KEY',
-        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+        callbackURL: 'http://gymwithmusic.herokuapp.com/auth/linkedin/callback'
     }
 };
